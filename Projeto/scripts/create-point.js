c0a374f4efd893edfd0016ad56517b620e1bbfd9
Luís Galete <luisgalete6264@gmail.com>
2020-06-04 13:51:58 -0300

    Fix bug 1-"Cidades duplicadas" & 2-"Required fields"

--- Projeto/scripts/create-point.js
+++ Projeto/scripts/create-point.js
@@ -23,11 +23,14 @@ function getCities(event) {
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+    
     fetch(url)
         .then(res => res.json())
         .then(cities => {
             for (const city of cities) {
-                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
             }
             citySelect.disabled = false
         })
