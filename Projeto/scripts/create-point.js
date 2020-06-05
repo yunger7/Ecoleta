4d5546d168d7c229c64a59d4a7a0e90b3c563288
Luís Galete <luisgalete6264@gmail.com>
2020-06-05 15:58:25 -0300

    Add "Itens de coleta"

--- Projeto/scripts/create-point.js
+++ Projeto/scripts/create-point.js
@@ -39,3 +39,45 @@ function getCities(event) {
 document
     .querySelector('select[name=uf]')
     .addEventListener("change", getCities)
+
+// Itens de coleta
+
+const itemsToCollect = document.querySelectorAll('.items-grid li')
+
+for (const item of itemsToCollect) {
+    item.addEventListener('click', handleSelectedItem)
+}
+
+const collectedItems = document.querySelector('input[name=items]')
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+
+    //add or remove class with javascript
+    itemLi.classList.toggle('selected')
+
+    const itemId = itemLi.dataset.id
+
+    //check if there is a selection, if yes, get those items 
+    const alreadySelected = selectedItems.findIndex(item => {
+        const itemFound = item == itemId //return true or false
+        return itemFound
+    })
+
+    //if selected, remove from selection
+    if(alreadySelected >= 0){
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+        })
+        selectedItems = filteredItems
+    } else {
+    //if not selected, add to selection 
+    selectedItems.push(itemId)
+    }
+
+    //reload hidden field with selected data
+    collectedItems.value = selectedItems
+}
