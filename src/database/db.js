8e57c75de5dbe8634abbba8b9df988474d0976f4
Luís Galete <luisgalete6264@gmail.com>
2020-06-06 20:12:38 -0300

    integrate html with database

--- src/database/db.js
+++ src/database/db.js
@@ -8,9 +8,9 @@ module.exports = db
 
 // utilizar o objeto de banco de dados para nossas operacoes
 // db.serialize(() => {
-//     // // com comandos SQL eu vou
+//     // com comandos SQL eu vou
 
-//     // // 1 criar uma tabela
+//     // 1 criar uma tabela
 //     // db.run(`
 //     //     CREATE TABLE IF NOT EXISTS places (
 //     //         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -24,7 +24,7 @@ module.exports = db
 //     //     );
 //     // `)
 
-//     // // 2 inserir dados na tabela
+//     // 2 inserir dados na tabela
 //     // const query = `
 //     //     INSERT INTO PLACES (
 //     //         image,
@@ -58,22 +58,22 @@ module.exports = db
 
 //     // db.run(query, values, afterInsertData)
 
-//     // // 3 consultar os dados da tabela
-//     // // db.all(`SELECT name FROM places`, function(err, rows){
-//     // //     if(err) {
-//     // //         return console.log(err)
-//     // //     }
+//     // 3 consultar os dados da tabela
+//     // db.all(`SELECT name FROM places`, function(err, rows){
+//     //     if(err) {
+//     //         return console.log(err)
+//     //     }
 
-//     // //     console.log("Aqui estao seus registros")
-//     // //     console.log(rows)
-//     // // })
+//     //     console.log("Aqui estao seus registros")
+//     //     console.log(rows)
+//     // })
 
-//     // // 4 deletar um dado da tabela
-//     // // db.run(`DELETE FROM places WHERE id = ?`, [1], function(err){
-//     // //     if(err) {
-//     // //         return console.log(err)
-//     // //     }
+//     // 4 deletar um dado da tabela
+//     // db.run(`DELETE FROM places WHERE id = ?`, [6], function(err){
+//     //     if(err) {
+//     //         return console.log(err)
+//     //     }
 
-//     // //     console.log("Registro deletado com sucesso!")
-//     // // })
+//     //     console.log("Registro deletado com sucesso!")
+//     // })
 // })
--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const server = express()
 
+// pegar o banco de dados
+const db = require("./database/db")
+
 // Configurar pasta publica
 server.use(express.static("public"))
 
@@ -25,7 +28,21 @@ server.get("/create-point", (req, res) => {
 })
 
 server.get("/search", (req, res) => {
-    return res.render("search-results.html")
+
+    // pegar os dados do banco de dados
+    db.all(`SELECT * FROM places`, function (err, rows) {
+        if (err) {
+            return console.log(err)
+        }
+
+        // console.log("Aqui estao seus registros")
+        // console.log(rows)
+
+        const total = rows.length
+
+        // mostrar a pagina html com os dados do banco de dados
+        return res.render("search-results.html", {places: rows, total})
+    })
 })
 
 // Ligar o servidor
